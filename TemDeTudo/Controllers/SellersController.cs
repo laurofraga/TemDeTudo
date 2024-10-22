12c575bfd4dcacb975ab3b2e2e1a655a6a8d2505
LAURO FRAGA DIAS <632210218@fspoa.br>
2024-10-21 21:46:04 -0300

    feat:Seller CRUD fully implemented

--- TemDeTudo/Controllers/SellersController.cs
+++ TemDeTudo/Controllers/SellersController.cs
@@ -50,5 +50,94 @@ namespace TemDeTudo.Controllers
 
           return RedirectToAction("Index");
         }
+
+        public IActionResult Details(int? id)
+        {   
+            //verifica se foi passado um id como parâmetro
+            if (id == null)
+            {
+                return NotFound();
+            }
+                                                   //x é a variável do vendedor e depois verifica se ele existe; 
+            Seller seller = _context.Seller.Include("Department").FirstOrDefault(x => x.Id == id);
+
+            //se não localizar vendedor com id, vai pra página de erro 
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+
+
+        }
+
+        public IActionResult Delete(int? id) 
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Seller  seller = _context.Seller.Include("Department").FirstOrDefault(x =>x.Id == id);
+            
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id) 
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Seller seller = _context.Seller.Include("Department").FirstOrDefault(x => x.Id == id);
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id) 
+        {
+            //Verificar se existe um vendedor com o id passado por parâmetro
+            var seller = _context.Seller.First(x => x.Id == id);
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            //Criar lista de departamentos
+            List<Department> departments = _context.Department.ToList();
+
+            //Cria instância do view model 
+            SellerFormViewModel viewModel = new SellerFormViewModel();
+            viewModel.Seller = seller;
+            viewModel.Departments = departments;
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Seller seller) 
+        {
+            _context.Update(seller);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
+
+
 }
--- TemDeTudo/Models/Seller.cs
+++ TemDeTudo/Models/Seller.cs
@@ -1,12 +1,27 @@
-﻿namespace TemDeTudo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TemDeTudo.Models
 {
     public class Seller
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Sellers Name")]
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BithDate { get; set; }
+
+        [Range(1400, 50000, ErrorMessage = "Invalid sallary")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public Decimal Salary { get; set; }
+        
         public int DepartmentId { get; set; }
         public Department Department{ get; set; }
 
